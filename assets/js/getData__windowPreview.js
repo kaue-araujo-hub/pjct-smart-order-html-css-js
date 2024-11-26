4e1a066f36fd8ac530ecd82258b124622451ca29
kaue-araujo-hub <kauearaujo_@outlook.com>
2024-11-26 20:40:17 -0300

    Refatorando -> windowPreview.html

---
+++ assets/js/getData__windowPreview.js
@@ -0,0 +1,15 @@
+
+
+
+
+document.getElementById('preview-local-origem-ida').textContent = localStorage.getItem('local-origem-ida');
+document.getElementById('preview-local-destino-ida').textContent = localStorage.getItem('local-destino-ida');
+document.getElementById('preview-data-volta').textContent = localStorage.getItem('data-volta');
+document.getElementById('preview-hora-inicio-volta').textContent = localStorage.getItem('hora-inicio-volta');
+document.getElementById('preview-hora-chegada-volta').textContent = localStorage.getItem('hora-chegada-volta');
+document.getElementById('preview-local-origem-volta').textContent = localStorage.getItem('local-origem-volta');
+document.getElementById('preview-local-destino-volta').textContent = localStorage.getItem('local-destino-volta');
+document.getElementById('preview-inputbox-inicio-servico').textContent = localStorage.getItem('inputbox-inicio-servico');
+document.getElementById('preview-inputbox-termino-servico').textContent = localStorage.getItem('inputbox-termino-servico');
+document.getElementById('preview-status-atend').textContent = localStorage.getItem('status-atend');
+document.getElementById('preview-inputbox-obs').textContent = localStorage.getItem('inputbox-obs');
