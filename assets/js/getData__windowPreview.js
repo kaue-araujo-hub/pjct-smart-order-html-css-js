0a7e70c345d99f61a5b1478f31b9f2c00c5e0695
kaue-araujo-hub <kauearaujo_@outlook.com>
2024-11-27 08:55:49 -0300

    Refatorando -> windowPreview.html (2)

--- assets/js/getData__windowPreview.js
+++ assets/js/getData__windowPreview.js
@@ -1,11 +1,16 @@
 
 
 
+// typeOfService__section
+document.getElementById('dateOfServiceInput__preview').textContent = localStorage.getItem('dateOfServiceInput');
+document.getElementById('numberOfOrderInput__preview').textContent = localStorage.getItem('numberOfOrderInput');
+document.getElementById('typeOfServiceInput__preview').textContent = localStorage.getItem('typeOfServiceInput');
+document.getElementById('equipmentBrandInput__preview').textContent = localStorage.getItem('equipmentBrandInput');
 
-document.getElementById('preview-local-origem-ida').textContent = localStorage.getItem('local-origem-ida');
-document.getElementById('preview-local-destino-ida').textContent = localStorage.getItem('local-destino-ida');
-document.getElementById('preview-data-volta').textContent = localStorage.getItem('data-volta');
-document.getElementById('preview-hora-inicio-volta').textContent = localStorage.getItem('hora-inicio-volta');
+
+
+
+
 document.getElementById('preview-hora-chegada-volta').textContent = localStorage.getItem('hora-chegada-volta');
 document.getElementById('preview-local-origem-volta').textContent = localStorage.getItem('local-origem-volta');
 document.getElementById('preview-local-destino-volta').textContent = localStorage.getItem('local-destino-volta');
