e3b1a18a3dcad122add03e2ed6992c61549da7c1
kaue-araujo-hub <kauearaujo_@outlook.com>
2024-11-29 16:51:14 -0300

    Refatorando -> windowPreview.html (4)

--- assets/js/getData__windowPreview.js
+++ assets/js/getData__windowPreview.js
@@ -15,3 +15,24 @@ document.getElementById('municipalityInput__preview').textContent = localStorage
 document.getElementById('ufStatesInput__preview').textContent = localStorage.getItem('ufStatesInput');
 document.getElementById('customerContactNameInput__preview').textContent = localStorage.getItem('icustomerContactNameInput');
 document.getElementById('customerContactNumberInput__preview').textContent = localStorage.getItem('customerContactNumberInput');
+
+// equipment__section
+document.getElementById('nameOfEquipmentInput__preview').textContent = localStorage.getItem('nameOfEquipmentInput');
+document.getElementById('serialNumberInput__preview').textContent = localStorage.getItem('serialNumberInput');
+document.getElementById('equipmentBrandInput__preview').textContent = localStorage.getItem('equipmentBrandInput');
+document.getElementById('equipStatusInput__preview').textContent = localStorage.getItem('equipStatusInput');
+
+
+// displacement__section to GO
+document.getElementById('departureDateInput__preview').textContent = localStorage.getItem('departureDateInput');
+document.getElementById('departureTimeToGoInput__preview').textContent = localStorage.getItem('departureTimeToGoInput');
+document.getElementById('arrivalTimeToGoInput__preview').textContent = localStorage.getItem('arrivalTimeToGoInput');
+document.getElementById('departureLocationToGoInput__preview').textContent = localStorage.getItem('departureLocationToGoInput');
+document.getElementById('arrivalLocationToGoInput__preview').textContent = localStorage.getItem('arrivalLocationToGoInput');
+
+// displacement__section to RETURN
+document.getElementById('returnDateInput__preview').textContent = localStorage.getItem('returnDateInput');
+document.getElementById('departureTimeToReturnInput__preview').textContent = localStorage.getItem('departureTimeToReturnInput');
+document.getElementById('arrivalTimeToReturnInput__preview').textContent = localStorage.getItem('arrivalTimeToReturnInput');
+document.getElementById('departureLocationToReturnInput__preview').textContent = localStorage.getItem('departureLocationToReturnInput');
+document.getElementById('arrivalLocationToReturnInput__preview').textContent = localStorage.getItem('arrivalLocationToReturnInput');
