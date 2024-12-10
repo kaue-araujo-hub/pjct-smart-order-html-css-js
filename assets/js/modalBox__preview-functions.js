8c16c34fdfd512f35c7acd117cb1a3ed254cc909
kaue-araujo-hub <kauearaujo_@outlook.com>
2024-12-10 18:19:32 -0300

    Implementando método de print no modal -> windowPreview.html (15)

--- assets/js/modalBox__preview-functions.js
+++ assets/js/modalBox__preview-functions.js
@@ -5,13 +5,6 @@ document.getElementById("previewButton").addEventListener("click", function() {
     document.getElementById("overlay").style.display = "block";
 });
 
-// Função para imprimir o conteúdo do iframe
-document.getElementById("printButton").addEventListener("click", function() {
-    const iframe = document.getElementById("previewFrame").contentWindow;
-    iframe.focus(); // Garante que o conteúdo do iframe está ativo
-    iframe.print(); // Imprime o conteúdo do iframe
-});
-
 document.getElementById("closeButton").addEventListener("click", function() {
     document.getElementById("previewModal").classList.remove("show");
     document.getElementById("overlay").style.display = "none";
